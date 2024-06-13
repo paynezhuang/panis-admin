94e9e2d8680220a78d5288b509e131c52274a52e
paynezhuang <paynezhuang@gmail.com>
2024-06-13 23:32:58 +0800

    perf(projects): update system title

--- src/locales/langs/en-us.ts
+++ src/locales/langs/en-us.ts
@@ -1,6 +1,6 @@
 const local: App.I18n.Schema = {
   system: {
-    title: 'SoybeanAdmin',
+    title: 'PanisAdmin',
     updateTitle: 'System Version Update Notification',
     updateContent: 'A new version of the system has been detected. Do you want to refresh the page immediately?',
     updateConfirm: 'Refresh immediately',
